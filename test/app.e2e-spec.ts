6dc77f58da6a47971b63dba903769767c582f2c1
Hifzur-rahman <55972952+Hifzur-rahman@users.noreply.github.com>
2021-08-18 22:32:13 +0530

    added test:e2e for all API

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -2,23 +2,64 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
+import { TodoModule } from '../src/todo/todo.module';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
+      imports: [AppModule, TodoModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
     await app.init();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
+  it('/ (GET)', async () => {
+    const test = await request(app.getHttpServer())
       .get('/')
-      .expect(200)
-      .expect('Hello World!');
+    expect(test.text).toBe('Hello World!')
   });
+
+
+  it('/todos (GET)', async () => {
+    const getAllData = await request(app.getHttpServer())
+      .get('/todos')
+    expect(getAllData.text)
+  });
+
+  it('/todos/:id (GET)', async () => {
+    const getIdbyUser = await request(app.getHttpServer())
+      .get('/todos/611bf6c5b5a8934b044708ae')
+    expect(getIdbyUser.body._id).toBe('611bf6c5b5a8934b044708ae')
+  });
+
+
+  it('/todos/:id (PUT)', async () => {
+    const putIdbyUser = await request(app.getHttpServer())
+      .put('/todos/611bf6c5b5a8934b044708ae').send({
+        "title": "test-e2e",
+        "description": "just a new todo",
+      })
+    expect(putIdbyUser.body._id).toBe('611bf6c5b5a8934b044708ae')
+  });
+
+
+  it('/todos (POST)', async () => {
+    const postBody = await request(app.getHttpServer())
+      .post('/todos').send({
+        "title": "test-e2e",
+        "description": "im testing e2e testcase",
+      })
+    expect(postBody.text)
+  });
+
+
+  it('/todos/:id (DELETE)', async () => {
+    const deletebyId = await request(app.getHttpServer())
+      .delete('/todos/611ccdd8039e282525f5084d')
+    expect(deletebyId.body._id).toBe('611ccdd8039e282525f5084d')
+  });
+
 });
